29c3669d2129116bf0985a4d8870d80377fdb91c
vincgonzo <vincgonzo@protonmail.com>
2023-03-14 21:12:08 +0100

    fix ( ASM ) : challenge 19

--- embryoasm_walk/embryoasm_19/asm_script.s
+++ embryoasm_walk/embryoasm_19/asm_script.s
@@ -1,44 +1,14 @@
 .intel_syntax noprefix
-# if [x] is 0x7f454c46:
-#   y = [x+4] + [x+8] + [x+12]
-# else if [x] is 0x5A4D:
-#   y = [x+4] - [x+8] - [x+12]
-# else:
-#   y = [x+4] * [x+8] * [x+12]
-
 _start:
-    mov ecx, DWORD PTR [rdi]
-    mov ebx, DWORD PTR [rdi + 0x4]
-    mov edx, DWORD PTR [rdi + 0x8]
-    mov esi, DWORD PTR [rdi + 0x12]
     int3
-    cmp ecx, 0x7f454c46
-    je calc_sum
-    sub cx, 0x5A4D 
-    je calc_diff
-    jmp calc_product
-
-calc_sum:
-    mov eax, [ebx]
-    add eax, [edx]
-    add eax, [esi]
+    cmp di, 3
     int3
-
-calc_diff:
-    mov eax, [ebx]
-    sub eax, [edx]
-    sub eax, [esi]
+    jna default
+    mov rdi, 4
+    
+    default:
+        lea rax, [rsi + rdi *8]
+        mov rax, [rax]
+        jmp rax
     int3
-
-calc_product:
-    mov rax, rbx
-    imul rax, rdx
-    cmp rsi, 0x0
-    jz exit
-    imul rax, rsi
-    int3
-
-exit:
-    nop
-    int 0x80
 
--- embryoasm_walk/embryoasm_19/ipython_script.py
+++ embryoasm_walk/embryoasm_19/ipython_script.py
@@ -3,32 +3,18 @@ import pwn
 pwn.context.update(arch="amd64")
 process=pwn.process("/challenge/run")
 process.write(pwn.asm("""
-    mov ecx, DWORD PTR [rdi]
-    mov ebx, DWORD PTR [rdi + 0x4]
-    mov edx, DWORD PTR [rdi + 0x8]
-    mov esi, DWORD PTR [rdi + 0x12]
-    mov eax, ebx
+    int3
+    cmp di, 3
+    int3
+    jna default
+    mov rdi, 4
+    
+    default:
+        lea rax, [rsi + rdi *8]
+        mov rax, [rax]
+        jmp rax
     int3
 
-    cmp ecx, 0x7f454c46
-    je calc_sum
-    cmp cx, 0x5A4D
-    je calc_diff
-    jmp calc_product
-    calc_sum: 
-        add rax, rdx
-        add rax, rsi
-        jmp exit
-    calc_diff: 
-        sub eax, edx
-        sub eax, esi
-        jmp exit
-    calc_product: 
-        imul rax, rdx
-        imul rax, rsi
-        jmp exit
-    exit:  
-        int3
     """))
 print(process.readallS())
 
