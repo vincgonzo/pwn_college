dc9f74f5f51a4e1b9d9eefb64a9e33600957a374
vincgonzo <vincgonzo@protonmail.com>
2023-03-16 22:10:44 +0100

    build ( build webserver ) : building bind

---
+++ build_webserver/build3/asm_script.s
@@ -0,0 +1,28 @@
+.intel_syntax noprefix
+.global _start
+
+.section .text
+
+_start:
+	mov rdi, 2 
+	mov rsi, 1 
+	mov rdx, 0
+	mov rax, 0x29 
+	syscall
+
+	mov rdi, 3 # page socket return 
+	lea rsi, [rip + sockaddr] 
+	mov rdx, 16	
+	mov rax, 0x31
+	syscall 
+
+	mov rdi, 0
+	mov rax, 60
+	syscall 
+
+.section .data
+sockaddr:
+	.2byte 2
+	.2byte 0x5000
+	.4byte 0
+	.8byte 0
