0b0f15ef90cbfe96c13666d01ad88923cae9c380
vincgonzo <vincgonzo@protonmail.com>
2023-03-17 17:25:18 +0100

    build ( BWS ) : challenge 4 init project

---
+++ build_webserver/build4/c_program.c
@@ -0,0 +1,37 @@
+#include <stdio.h>          /* See NOTES */
+#include <sys/types.h>          /* See NOTES */
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> /* superset of previous */
+
+int main()
+{
+    int socket_desc;
+    struct sockaddr_in server;
+
+    // Create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        printf("Could not create socket");
+        return 1;
+    }
+
+    // Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8080);
+
+    // Bind
+    if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        puts("bind failed");
+        return 1;
+    }
+    puts("bind done");
+
+    // Listen
+    listen(socket_desc, 0);
+
+    // Accept incoming connection
+    puts("Waiting for incoming connections...");
+    return 0;
+}
